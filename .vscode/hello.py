e61c768c62b410ce277eaae3bfcaa86fb0fc9f73
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2024-10-26 00:30:50 +0200

    C++_python

---
+++ .vscode/hello.py
@@ -0,0 +1,8 @@
+# print a welcome message
+print('Hello world')
+
+#ask the user for their name
+user_name = input("what is your name")
+
+#print the user with a welcome message
+print("welcome" + user_name + " to python programming!")
