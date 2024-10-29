aae1160c5dd34055417d34d5f6ec1ae1bd6611cc
SALONI-MELKUNDE <salonimelkunde@gmail.com>
2024-10-29 17:48:13 +0100

    deleted files

--- Weekwiseclasswork/demo.py
+++
@@ -1,6 +0,0 @@
-
-print([p["name"] for p in sorted([{"name": "Alice", "age": 30}, 
-{"name": "Bob", "age": 25}, {"name": "Charlie", "age": 35}, {"name": "David", "age": 20}], key=lambda x: x["age"])])
-
-
-
